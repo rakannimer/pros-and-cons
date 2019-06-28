fdddab2d36993381828a9d2e6ce6739f2298c0f6
rakannimer <rakannimer@gmail.com>
2019-06-28 08:17:03 +0300

    styled-components to plain old CSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,155 +1,88 @@
 import * as React from "react";
 import { render } from "react-dom";
 
-import styled from "styled-components";
-
-import {
-  AppContainer,
-  LeftSidebar,
-  LeftSidebarHome,
-  PageHeader,
-  ProjectTitleAndShareContainer,
-  ProsAndConsAndRightSidebar,
-  ProsAndConsContainer,
-  ProsList,
-  ConsList,
-  RightSidebar,
-  AppWithoutLeftSidebar,
-  ListTitle,
-  ListItems,
-  ListItemContainer,
-  ArgumentDescriptionContainer,
-  ArgumentWeightContainer,
-  ArgumentDeleteContainer
-} from "./atoms";
-
 import "./styles.css";
-import { colors } from "./colors";
 import { range } from "./utils";
 
 function ListItem() {
   return (
-    <ListItemContainer>
-      <ArgumentDescriptionContainer
+    <div className="list-item-container">
+      <div
+        className="argument-description-container"
         onInput={ev => {
           console.log("Value is ", ev.currentTarget.innerText);
         }}
         contentEditable
       >
         Lorem Ipsum Dorum jhjhjhsdsdsdsds
-      </ArgumentDescriptionContainer>
-      <ArgumentWeightContainer>
+      </div>
+      <div className="argument-weight-container">
         <select>
           {range(1, 10).map(w => (
             <option key={w}>{w}</option>
           ))}
         </select>
-      </ArgumentWeightContainer>
-      <ArgumentDeleteContainer>
+      </div>
+      <div className="argument-delete-container">
         <div>x</div>
-      </ArgumentDeleteContainer>
-    </ListItemContainer>
+      </div>
+    </div>
   );
 }
 
-const ListFooter = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  /* background: violet; */
-  /* height: 100%; */
-  /* min-height: 60px; */
-  min-height: 8vh;
-  width: 100%;
-  bottom: 0;
-`;
-
-const AddArgumentButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const ProjectTitleContainer = styled.div`
-  flex: 6;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: bold;
-`;
-const ExportButtonContainer = styled.button`
-  flex: 0.8;
-  background: inherit;
-  color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: solid ${colors.white} 1px;
-  height: 5vh;
-  border-radius: 50px;
-  margin-right: 10px;
-  font-family: "Roboto", sans-serif;
-  /* font-weight: bold; */
-`;
-
-const ListItemsAndFooter = styled.div`
-  width: 100%;
-  background: ${colors.gray};
-`;
-
 function App() {
   return (
-    <AppContainer>
-      <LeftSidebar>
-        <LeftSidebarHome>H</LeftSidebarHome>
-      </LeftSidebar>
-      <AppWithoutLeftSidebar>
-        <PageHeader>PROS & CONS</PageHeader>
-        <ProjectTitleAndShareContainer>
-          <ProjectTitleContainer>Lorem Ipsum</ProjectTitleContainer>
-          <ExportButtonContainer>
-            Export
+    <div className="app-container">
+      <div className="left-sidebar">
+        <div className="left-sidebar-home-button">H</div>
+      </div>
+      <div className="app-without-left-sidebar">
+        <div className="app-header">PROS & CONS</div>
+        <div className="title-and-share-container">
+          <div className="title-container">Lorem Ipsum</div>
+          <div className="share-button-container">
+            Share
             <div>+</div>
-          </ExportButtonContainer>
-        </ProjectTitleAndShareContainer>
-        <ProsAndConsAndRightSidebar>
-          <ProsAndConsContainer>
-            <ProsList>
-              <ListTitle>PROS</ListTitle>
-              <ListItemsAndFooter>
-                <ListItems>
+          </div>
+        </div>
+        <div className="pros-and-cons-and-right-sidebar">
+          <div className="pros-and-cons-container">
+            <div className="list">
+              <div className="list-title">PROS</div>
+              <div className="list-items-and-footer">
+                <div className="list-items">
                   <ListItem />
                   <ListItem />
-                </ListItems>
-                <ListFooter>
-                  <AddArgumentButton>
+                </div>
+                <div className="list-footer">
+                  <div className="add-argument-button">
                     <div>icon</div>
                     <div>Add</div>
-                  </AddArgumentButton>
-                </ListFooter>
-              </ListItemsAndFooter>
-            </ProsList>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-            <ConsList>
-              <ListTitle>CONS</ListTitle>
-              <ListItemsAndFooter>
-                <ListItems>
+            <div className="list">
+              <div className="list-title">CONS</div>
+              <div className="list-items-and-footer">
+                <div className="list-items">
                   <ListItem />
                   <ListItem />
-                </ListItems>
-                <ListFooter>
-                  <AddArgumentButton>
+                </div>
+                <div className="list-footer">
+                  <div className="add-argument-button">
                     <div>icon</div>
                     <div>Add</div>
-                  </AddArgumentButton>
-                </ListFooter>
-              </ListItemsAndFooter>
-            </ConsList>
-          </ProsAndConsContainer>
-          <RightSidebar>Sidebar</RightSidebar>
-        </ProsAndConsAndRightSidebar>
-      </AppWithoutLeftSidebar>
-    </AppContainer>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right-sidebar">Sidebar</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
