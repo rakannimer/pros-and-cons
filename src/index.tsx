60fd589a175ee524174934a49071173e97e29daa
rakannimer <rakannimer@gmail.com>
2019-07-10 11:21:49 +0300

    Re-enable service worker

--- src/index.tsx
+++ src/index.tsx
@@ -89,7 +89,7 @@ render(<App />, rootElement);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 // serviceWorker.register({
 //   onUpdate: registration => {
 //     console.warn("onUpdate");
